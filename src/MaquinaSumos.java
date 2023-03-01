39efe845d826a73c8ce853d33d7be5b545ac0325
bakerfella <daniel.santos.ds622@gmail.com>
2023-03-01 18:02:20 +0000

    last commit 01-03

---
+++ src/MaquinaSumos.java
@@ -0,0 +1,16 @@
+public class MaquinaSumos {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        }
+        public static void testValues() {
+            Product cocacola = new Product("Coca-cola", 0.70, 5);
+            Product pepsi = new Product("Pepsi", 0.65, 7);
+            Product icetea = new Product("Ice Tea", 0.60, 4);
+        }
+    }
