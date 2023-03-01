69ef8d4f506e8b6f1ee169e5758f8d8b04115d6e
bakerfella <daniel.santos.ds622@gmail.com>
2023-03-01 16:53:19 +0000

    produto commit

---
+++ src/Produto.java
@@ -0,0 +1,22 @@
+public class Produto {
+
+    private String name;
+    private double price;
+
+    public Produto(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
